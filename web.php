31a356446a3d834091295d393db6eefbe786ff92
miftahul-huda27 <miftah27403@gmail.com>
2025-04-29 22:18:59 +0700

    web.php

--- web.php
+++ web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BiodataController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('ambilfile');
 });
 
 Route::get('ambilfile', [BiodataController::class, 'ambilfile']);
